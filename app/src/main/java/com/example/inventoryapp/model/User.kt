5e5d1d41c7ed1ee06dff9bc9e398fea8fbacb0dc
Ovett Mora <ovettmora17@gmail.com>
2025-06-11 22:21:06 -0600

    Change user ID type from Long to String for UUID support

--- app/src/main/java/com/example/inventoryapp/model/User.kt
+++ app/src/main/java/com/example/inventoryapp/model/User.kt
@@ -1,7 +1,9 @@
 package com.example.inventoryapp.model
 
+import java.util.UUID
+
 class User {
-    var id: Long? = null
+    var id: String? = null
     var name: String? = null
     var email: String? = null
     var profilePicture: String? = null
